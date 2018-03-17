67f21b1805a97732e8b441448aed135f0458dc58
TheUnlocked <>
2018-03-17 14:42:03 -0700

    Added publish bat

---
+++ FAILang/publish.bat
@@ -0,0 +1,4 @@
+dotnet publish -c Release --self-contained -r win-x64
+dotnet publish -c Release --self-contained -r linux-x64
+dotnet publish -c Release --self-contained -r osx.10.12-x64
+dotnet publish -c Release
